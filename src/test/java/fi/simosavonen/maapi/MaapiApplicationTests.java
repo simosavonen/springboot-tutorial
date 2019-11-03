ddc7d6827e85d6164f52fa9a08a9e4932da767af
Simo Savonen <sipesa@utu.fi>
2019-11-03 21:25:38 +0200

    end of part 1

---
+++ src/test/java/fi/simosavonen/maapi/MaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.simosavonen.maapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
